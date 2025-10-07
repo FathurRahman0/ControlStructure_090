1078958c826229a9a471828a6b7c852b0c69b03d
FathurRahman0 <rahmanfthr165@gmail.com>
2025-10-07 15:11:17 +0700

    menentukan angka terbesar

--- prak2.py
+++ prak2.py
@@ -1,3 +1,10 @@
 a = float(input("Masukkan angka pertama: "))
 b = float(input("Masukkan angka kedua: "))
 c = float(input("Masukkan angka ketiga: "))
+
+if a > b and a > c:
+    largest = a
+elif b > a and b > c:
+    largest = b
+elif c > a and c > b:
+    largest = c
